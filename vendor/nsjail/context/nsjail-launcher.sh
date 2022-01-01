b9da7cd4f8b55260876246e72de240cdbeed328c
tom <tom@ceebs.dev>
2022-01-01 21:20:37 +1100

    Fix up launcher to use source and move original process to cgroup

--- vendor/nsjail/context/nsjail-launcher.sh
+++ vendor/nsjail/context/nsjail-launcher.sh
@@ -2,5 +2,5 @@
 
 # this needs to run after /docker-init/docker-entrypoint.sh
 
-/docker-init/nsjail-config-cgroups.sh
+source /docker-init/nsjail-config-cgroups.sh
 exec runuser -u ctf -- /docker-init/nsjail-user.sh

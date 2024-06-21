97813e0cdad66b0e0beba9ad3c7faf60809cc1ec
tom <tom@ceebs.dev>
2024-06-21 23:52:18 +1000

    Use GitHub Actions cache API for build image caching

--- scripts/build-and-push-changed.sh
+++ scripts/build-and-push-changed.sh
@@ -7,8 +7,8 @@ for project in `get_changed_compose_projects`; do
 (
     cd "$project"
     docker buildx bake --push \
-        --set '*.cache-from=type=local,src=/tmp/.buildx-cache' \
-        --set '*.cache-to=type=local,mode=max,dest=/tmp/.buildx-cache-new' \
+        --set '*.cache-from=type=gha' \
+        --set '*.cache-to=type=gha,mode=max' \
         -f docker-compose.yml
 )
 done
